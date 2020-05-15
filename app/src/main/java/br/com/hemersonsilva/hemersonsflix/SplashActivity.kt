50d3981fd8e86b8f0696d68cfe80c369971a6f2f
Hemerson Silva <hemersonoas@gmail.com>
2020-05-15 15:11:08 -0300

    Adicionando Splash Screen

---
+++ app/src/main/java/br/com/hemersonsilva/hemersonsflix/SplashActivity.kt
@@ -0,0 +1,27 @@
+package br.com.hemersonsilva.hemersonsflix
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import br.com.hemersonsilva.hemersonsflix.ui.activity.MainActivity
+
+class SplashActivity : AppCompatActivity(), Runnable {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setDelay()
+
+    }
+
+    private fun setDelay() {
+        val handler = Handler()
+        handler.postDelayed(this, 2000)
+    }
+
+    override fun run() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
